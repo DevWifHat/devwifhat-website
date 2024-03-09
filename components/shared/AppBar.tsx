eef2a3b7dbe0bb8dc93bc00604f902d79a943ebe
Nyk <0xnykcd@googlemail.com>
2024-03-09 10:54:31 +0100

    navbar link

--- components/shared/AppBar.tsx
+++ components/shared/AppBar.tsx
@@ -81,10 +81,11 @@ export default function Example() {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="https://t.co/lT4QYJIuYR"
+                  target='_blank'
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
                 >
-                  Log in
+                  Join Now
                 </a>
               </div>
             </div>
