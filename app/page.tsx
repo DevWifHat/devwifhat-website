4d7d249e22029385d0de5c2461a411af84b544c9
Kulture <tiziankulture@proton.me>
2024-03-09 13:13:36 +0400

    add token infos

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,15 @@ import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
 import { faTelegram, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import useTokenData from '@/hooks/useTokenData';
+import { Spin } from 'antd';
 
 export default function Home() {
   const [isMuted, setIsMuted] = useState(true); // Initial state is muted
   const videoRef = useRef(null); // Reference to the video element
+
+  const { holders, price, currentSupply, isLoading, error } = useTokenData();
+  console.log("Holders: ", holders);
 
   // Function to toggle mute state
   const toggleMute = () => {
@@ -52,19 +57,54 @@ export default function Home() {
 
       {/* Section 3: Video inside a transparent frame */}
       <div className="mt-12 flex justify-center bg-contain bg-no-repeat h-125 relative">
-        <video ref={videoRef} className="mt-25" style={{ maxWidth: '55%', height: 'auto' }} autoPlay muted={isMuted} loop>
-          <source src="dev_wif_hat_teaser.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        {/* Wrapper div for video and button */}
+        <div className="relative" style={{ maxWidth: '55%' }}>
+          <video ref={videoRef} className="mt-25" style={{ width: '100%', height: 'auto' }} autoPlay muted={isMuted} loop>
+            <source src="dev_wif_hat_teaser.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          {/* Mute/Unmute Button positioned relative to the wrapper div */}
+          <div className="absolute bottom-0 left-0 text-center my-5 ml-5">
+            <button onClick={toggleMute} className="text-lg">
+              <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} size="2x" />
+            </button>
+          </div>
+        </div>
         <img src="Dev_Wif_Hat_Frame.png" alt="Frame" className="absolute top-0 left-1/2 transform -translate-x-1/2" style={{ maxWidth: '55%', height: 'auto' }} />
       </div>
 
-      {/* Mute/Unmute Button */}
-      <div className="text-center my-5">
-        <button onClick={toggleMute} className="text-lg">
-          <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} size="2x" />
-        </button>
+      {/* Section 4: Div aligned below the video, split into 4 quadrants */}
+      <div className="flex justify-center my-5">
+        <div style={{ maxWidth: '55%' }} className="w-full">
+          <div className="flex flex-wrap justify-between bg-black">
+            <div className="w-1/2 flex justify-center items-center border p-4">
+              <div>
+                <h1>{price.toFixed(7)}</h1>
+                <p>per $DGM</p>
+              </div>
+            </div>
+            <div className="w-1/2 flex justify-center items-center border p-4">
+              <div>
+                <h1>{isLoading ? <Spin /> : holders}</h1>
+                <p>Holders</p>
+              </div>
+            </div>
+            <div className="w-1/2 flex justify-center items-center border p-4">
+              <div>
+                <h1>760,000,000</h1>
+                <p>Initial Supply</p>
+              </div>
+            </div>
+            <div className="w-1/2 flex justify-center items-center border p-4">
+              <div>
+                <h1>{Math.round(currentSupply / 10e5).toLocaleString()}</h1>
+                <p>Current Supply</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
     </div>
   );
 }
