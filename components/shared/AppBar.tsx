90d5e734bcf4b55aa114388b575b403f7022cff7
Nyk <0xnykcd@googlemail.com>
2024-03-09 11:14:56 +0100

    black

--- components/shared/AppBar.tsx
+++ components/shared/AppBar.tsx
@@ -47,7 +47,7 @@ export default function Example() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5 flex flex-row items-center justify-center gap-2">
               <img src="dev_wif_hat_icon.png" alt="dev wif hat" className="w-14 h-14 mx-auto" />
