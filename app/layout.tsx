4764489a9c25af899afa2b6da973ef7a60c799ce
Nyk <0xnykcd@googlemail.com>
2024-03-12 19:22:06 +0100

    charts

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           {children}
         </div>
         <Footer />
-        <Toaster />
+        <Toaster theme='dark' />
       </body>
     </html>
   );
