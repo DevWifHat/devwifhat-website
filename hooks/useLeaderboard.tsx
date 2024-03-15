d26f86d95b273e2be0823a4d7823678c03b831e2
Kulture <tiziankulture@proton.me>
2024-03-15 13:01:58 +0400

    burnboard ready

--- hooks/useLeaderboard.tsx
+++ hooks/useLeaderboard.tsx
@@ -8,6 +8,8 @@ const useLeaderboard = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
 
+  const [index, setIndex] = useState(0);
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -22,9 +24,13 @@ const useLeaderboard = () => {
     };
 
     fetchData();
-  }, []);
+  }, [index]);
 
-  return { leaderboard, isLoading, error };
+  const reload = () => {
+    setIndex((prev) => prev + 1);
+  };
+
+  return { leaderboard, isLoading, error, reload };
 };
 
 export default useLeaderboard;
