81999890e8934eb189534802a38cc0611a3dca6c
Yann <yann.thibault1@gmail.com>
2024-03-28 00:26:19 +0400

    add slippage modal

--- app/onlyup/page.tsx
+++ app/onlyup/page.tsx
@@ -10,6 +10,8 @@ import { toast } from 'sonner';
 import { Spin } from 'antd';
 import { Connection, Keypair, VersionedTransaction } from '@solana/web3.js';
 import Modal from '@/components/shared/Modal';
+import { getTokenFromGecko } from '@/actions/fetchTokenFromGecko';
+import useTokenData from '@/hooks/useTokenData';
 
 // It is recommended that you use your own RPC endpoint.
 // This RPC endpoint is only for demonstration purposes so that this example will run.
@@ -28,7 +30,9 @@ export default function OnlyUp() {
   const [output, setOuput] = useState<number | string>("");
   const [slippage, setSlippage] = useState<number | string>(100);
   const [popUp, setPopUp] = useState(false);
-  
+  const [estimatedOutput, setEstimatedOutput] = useState<number | string>("");
+  const { price, isLoading } = useTokenData();
+
   useEffect(() => {
     if(Number(amount) > 0) {
       fetchQuote();
@@ -59,13 +63,13 @@ export default function OnlyUp() {
       ).json();
       console.log({ quoteResponse })
       setOuput(quoteResponse.outAmount / 10 ** 6);
+      setEstimatedOutput(Number(price * (quoteResponse.outAmount / 10 ** 6)).toFixed(2))
       } catch (error) {
       toast.error((error as Error).message);
     } finally {
       setLoading(false)
     }
   }
-  
 
   const handleSwap = async () => {
     if (!wallet.publicKey) {
@@ -84,7 +88,7 @@ export default function OnlyUp() {
 
     try {
     const quoteResponse = await (
-        await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=DEVwHJ57QMPPArD2CyjboMbdWvjEMjXRigYpaUNDTD7o&amount=${numericAmount}&slippageBps=100`)
+        await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=DEVwHJ57QMPPArD2CyjboMbdWvjEMjXRigYpaUNDTD7o&amount=${numericAmount}&slippageBps=${slippage}`)
     ).json();
 
     setOuput(quoteResponse.outputAmount);
@@ -108,7 +112,7 @@ export default function OnlyUp() {
             // feeAccount: "fee_account_public_key"
             // prioritizationFeeLamports: "auto",
             prioritizationFeeLamports: {
-              autoMultiplier: 2,
+              autoMultiplier: 2, // Increase that multiplier to increase the fee but that could be expensive
             },
       // feeAccount: "fee_account_public_key"
         })
@@ -118,10 +122,8 @@ export default function OnlyUp() {
     // deserialize the transaction
     const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
     var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-    console.log(transaction);
 
     // sign the transaction
-
     if(wallet && wallet.signTransaction) {
         // sign the transaction
         const signedTx = await wallet.signTransaction(transaction);
@@ -154,9 +156,9 @@ export default function OnlyUp() {
         <div className="flex flex-col items-center justify-center gap-6 px-4">
         <div className="flex justify-start text-slate-300 w-full">
           <div className="w-2/4 lg:w-1/3 mx-auto">
-            <div className="flex flex-col justify-between bg-slate-300 p-4 rounded-t-2xl border border-slate-300 h-1/2">
+            <div className="flex flex-col justify-between bg-slate-100 p-4 rounded-t-2xl border border-slate-300 h-1/2">
               <div className="flex justify-between">
-                <span className="text-slate-900">OnlyUp</span>
+                <span className="text-lg text-slate-900">OnlyUp</span>
                 <div className="flex space-x-4 justify-end">
                   <div className="grid grid-flow-col gap-4">
                     <button type="button" 
@@ -166,35 +168,7 @@ export default function OnlyUp() {
                       className="flex items-center space-x-1 text-slate-900 hover:animate-spin-slow min-w-5 min-h-5 group relative focus:outline-none border:none">
                     <span className="rounded-full absolute inset-0 -ml-1 -mr-1 -mb-1 -mt-1 group-hover:bg-slate-900 group-hover:bg-opacity-[0.08]"></span>
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_10794_148239)"><path d="M10.7997 7.8H10.7156C10.4516 6.768 9.51561 6 8.39961 6C7.28361 6 6.34757 6.768 6.08361 7.8H1.19961C0.863609 7.8 0.599609 8.064 0.599609 8.4C0.599609 8.736 0.863609 9 1.19961 9H6.08361C6.34761 10.032 7.28361 10.8 8.39961 10.8C9.51561 10.8 10.4517 10.032 10.7156 9H10.7997C11.1357 9 11.3997 8.736 11.3997 8.4C11.3997 8.064 11.1357 7.8 10.7997 7.8ZM8.39965 9.6C7.73963 9.6 7.19965 9.06002 7.19965 8.4C7.19965 7.73998 7.73963 7.2 8.39965 7.2C9.05967 7.2 9.59965 7.73998 9.59965 8.4C9.59965 9.06002 9.05967 9.6 8.39965 9.6Z" fill="currentColor"></path><path d="M1.19961 4.19995H1.28365C1.54765 5.23195 2.48365 5.99995 3.59965 5.99995C4.71565 5.99995 5.65169 5.23195 5.91565 4.19995H10.7997C11.1357 4.19995 11.3997 3.93595 11.3997 3.59995C11.3997 3.26395 11.1357 2.99995 10.7997 2.99995H5.91565C5.65165 1.96795 4.71565 1.19995 3.59965 1.19995C2.48365 1.19995 1.54761 1.96795 1.28365 2.99995H1.19961C0.863609 2.99995 0.599609 3.26395 0.599609 3.59995C0.599609 3.93595 0.863609 4.19995 1.19961 4.19995ZM3.59961 2.39995C4.25963 2.39995 4.79961 2.93993 4.79961 3.59995C4.79961 4.25997 4.25963 4.79995 3.59961 4.79995C2.93959 4.79995 2.39961 4.25997 2.39961 3.59995C2.39961 2.93993 2.93959 2.39995 3.59961 2.39995Z" fill="currentColor"></path></g><defs><clipPath id="clip0_10794_148239"><rect width="12" height="12" fill="currentColor"></rect></clipPath></defs></svg>
-                      <p>{slippage}%</p>
-                    </button>
-                  
-                  
-                    <button
-                      type="button"
-                      className="flex items-center text-slate-900 hover:animate-spin-slow min-w-5 min-h-5 group relative focus:outline-none border:none">
-                      <span className="rounded-full absolute inset-0 -ml-1 -mr-1 -mb-1 -mt-1 group-hover:bg-slate-900 group-hover:bg-opacity-[0.08]"></span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        width="20"
-                        height="20"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                        ></path>
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        ></path>
-                      </svg>
+                      <p>{Number(slippage) / 100}%</p>
                     </button>
                   </div>
                 </div>
@@ -210,7 +184,6 @@ export default function OnlyUp() {
                 onFocus={(e) => e.target.value === '1000000' ? setAmount('') : null} // Clear the input when focused if the value is '1000'
                 onBlur={(e) => e.target.value === '' ? setAmount('1000') : null} // Reset to '1000' if input is left empty
               />
-              <p className='text-slate-800'>estimated price: 900$</p>
             </div>
             <div className="flex items-center justify-center -mt-[12px] -mb-[12px] z-10">
               <button
@@ -244,7 +217,7 @@ export default function OnlyUp() {
                 value={output}
                 readOnly            
               />
-            <p className='text-slate-300'>estimated price: 900$</p>
+            <p className='px-6 text-slate-300 text-sm'>estimated price: {estimatedOutput}$</p>
               {/* <button
                 className="bg-blue-500 rounded-xl p-2 mx-auto my-4 w-full"
                 onClick={swap}
@@ -260,7 +233,7 @@ export default function OnlyUp() {
             </a>
           </button> */}
               <div className='mt-2 flex flex-col items-start justify-start gap-2'>
-              {wallet.publicKey ? <button onClick={handleSwap} className='bg-black w-full border border-white rounded-xl btn'>{loading && <Spin />} BUY</button> : <div className="w-full flex items-center justify-center border border-white opacity-50 rounded-xl"><WalletMultiButtonNoSSR /></div>}
+              {wallet.publicKey ? <button onClick={handleSwap} className='bg-emerald-500 text-xl text-bold hover:bg-emerald-400 text-slate-800 w-full rounded-xl btn'>{loading && <Spin />} 🚀 ROCKET BUY 🚀</button> : <div className="w-full flex items-center justify-center border border-white opacity-50 rounded-xl"><WalletMultiButtonNoSSR /></div>}
             </div>
             </div>
         </div>
--- components/shared/Modal.tsx
+++ components/shared/Modal.tsx
@@ -1,22 +1,67 @@
+import { useState } from "react";
+
 function Modal({setPopUp, setSlippage} : any) {
+  const [customSlippage, setCustomSlippage] = useState<number | string>(100);
 
   return (
     <div className='w-screen h-screen bg-black bg-opacity-90 fixed top-0 right-0 flex justify-center items-center'>
       <div className='bg-black border rounded-xl p-10 rounded-md shadow-md z-10'>
         <h1 className='font-bold text-center text-lg my-5'>Slippage Settings</h1>
-        <div className="w-full flex flex-col items-start justify-start gap-2 mb-4 ">
-              <span className='opacity-50 text-lg'>Enter Amount</span>
-              <input
+        <div className="w-full flex flex-col items-start justify-start gap-2 mb-4 bg-slate-900">
+              <div
+                className="inline-flex rounded-md shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-1 dark:focus:shadow-dark-1 dark:active:shadow-dark-1"
+                role="group">
+                <button
+                  type="button"
+                  onClick={() => setCustomSlippage(30)}
+                  className="inline-block focus:border focus:border-white rounded-s bg-primary-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-300 focus:bg-primary-accent-300 focus:outline-none focus:ring-0 active:bg-primary-600 motion-reduce:transition-none"
+                  data-twe-ripple-init
+                  data-twe-ripple-color="light">
+                  0.3%
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setCustomSlippage(50)}
+                  className="inline-block focus:border focus:border-white rounded-s bg-primary-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-300 focus:bg-primary-accent-300 focus:outline-none focus:ring-0 active:bg-primary-600 motion-reduce:transition-none"
+                  data-twe-ripple-init
+                  data-twe-ripple-color="light">
+                  0.5%
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setSlippage(100)}
+                  className="inline-block  focus:border focus:border-white rounded-s bg-primary-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-300 focus:bg-primary-accent-300 focus:outline-none focus:ring-0 active:bg-primary-600 motion-reduce:transition-none"
+                  data-twe-ripple-init
+                  data-twe-ripple-color="light">
+                  1%
+                </button>
+                {/* <button
+                  type="button"
+                  className="inline-block focus:border focus:border-white rounded-s bg-primary-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-300 focus:bg-primary-accent-300 focus:outline-none focus:ring-0 active:bg-primary-600 motion-reduce:transition-none"
+                  data-twe-ripple-init
+                  data-twe-ripple-color="light">
+                    <div className="inline-flex item-start justify-center"><span>Custom</span><input
                 type="number"
-                className="w-full input input-bordered rounded-xl py-2 px-6 mb-2 bg-black border border-white border-opacity-50"
+                className="w-full input focus:border focus:border-white py-2 px-6 mb-2 bg-black"
                 min="1"
                 max="10"
                 placeholder="1" // Placeholder shown when input is empty 
                 onChange={(e) => setSlippage(e.target.value)}       
-              />
+              /></div>
+                </button> */}
+
+                <div className="flex items-center justify-between focus:border focus:border-white cursor-text p-2 w-[130px] h-full text-black-50">
+                  <span className="text-xs lg:text-sm">Custom</span>
+                  <input inputMode="decimal" className="outline-none h-full w-full bg-transparent py-4 px-2 text-sm rounded-lg placeholder:text-white text-black-50 text-right pointer-events-all" placeholder="0.00%" type="text" 
+                  onChange={(e) => {
+                      setCustomSlippage(Number(e.target.value) * 100)
+                  }} />
+                </div>
+              </div>
+              
             </div>
         <div className='flex justify-between mt-5'>
-          <button onClick={() => setPopUp(false)} className='bg-black w-full border border-white rounded-xl btn'>
+          <button onClick={() => {setPopUp(false); setSlippage(Number(customSlippage))}} className='bg-black w-full border border-white rounded-xl btn'>
             Save Settings
           </button>
         </div>
@@ -24,5 +69,5 @@ function Modal({setPopUp, setSlippage} : any) {
     </div>
   )
 }
-
+    
 export default Modal
